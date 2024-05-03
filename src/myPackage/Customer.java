9754efaf59ff69a70866217e8bfca7a6482f136e
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-05-03 10:29:32 +0400

    minor fix

--- src/myPackage/Customer.java
+++ src/myPackage/Customer.java
@@ -3,6 +3,7 @@ package myPackage;
 public class Customer extends Person {
 	public Customer(String name, String lastName, String address, int age) throws Exception{
 		super(name, lastName, address, age);
+		//fff
 	}
 }
 
